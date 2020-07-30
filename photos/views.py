aa4a9cc757a5d9479f4e829cbffe6ef21d644f82
Maureen435 <mchepkemoi435@gmail.com>
2020-07-30 11:30:21 +0300

    added classes to view

--- photos/views.py
+++ photos/views.py
@@ -7,3 +7,21 @@ def home(request):
     images = Images.objects.all()
     return render(request, "home.html", {"images":images})
 
+class SearchResultsListView(ListView):
+    model = Images 
+    context_object_name = 'images_list'
+    template_name = 'search.html'
+
+    def get_query(self):
+        query = self.request.GET.get('q')
+        if Images.objects.filter(q(image_category=query))
+            return Images.objects.filter(Q(image_category=query))
+class SearchLocationListView(ListView):
+    model = Images 
+    context_object_name = 'images_list'
+    template_name = 'location.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if Images.objects.filter(Q(image_location=query))
+            return Images.objects.filter(Q(image_location=query))
