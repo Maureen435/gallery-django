c28ed66c0353528771ecf8c34bbdae9a0017e0a1
Maureen435 <mchepkemoi435@gmail.com>
2020-07-30 11:40:46 +0300

    modified urls.py file

--- photos/urls.py
+++ photos/urls.py
@@ -4,3 +4,11 @@ from django.conf.urls.static import static
 from . import views
 from .views import SearchResultsListView, SearchLocationListView
 
+urlpatterns=[
+    path('', Views.home,name='home'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
+    path('search/', SearchLocationListView, name='search_by_location'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=setttings.MEDIA_ROOT)
