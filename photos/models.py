b783805afa30b5a0e1d8d9d15bdea8669fe9ba28
Maureen435 <mchepkemoi435@gmail.com>
2020-07-31 09:38:14 +0300

    added classes to models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Images(models.Model):
+    name = models.CharField(max_length = 60)
+    image = models.ImageField(upload_to = 'images/')
+    description = models.TextField()
+    image_location = models.ForeignKey('location', on_delete=models.CASCADE,)
+    image_category = models.ForeignKey('Category', on_delete=models.CASCADE,)
+
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(image_category_name = category )
+        return images
