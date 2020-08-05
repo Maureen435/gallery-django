297792b7e1638f11288bf3c820a1a29b1cc59e63
Maureen435 <mchepkemoi435@gmail.com>
2020-08-05 10:46:22 +0300

    added images

--- gallery/settings.py
+++ gallery/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3'
+    'bootstrap3',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
